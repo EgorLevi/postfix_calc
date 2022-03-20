6fecc2559b5bb3a3a8fb526489a2982f345ac762
caps <caps@DESKTOP-C3U4CNO>
2022-03-20 18:33:22 +0200

    dat shit is soesent work mf

--- main.py
+++ main.py
@@ -1,34 +1,65 @@
 
-B =   ['2', '+', '2', '*', '2']
+B =   ['2', '+', '2', '*', '2', '+', '2', '*', '2']
 priority = {
     '+':1,
-    '*':2
+    '-':1,
+    '*':2,
+    '/':2
 }
+oper = ['+', '*', '-', '/']
 
 def stack_is_empty(stack):
     if len(stack) == 0:
         return True
     return False
 
+def get_stack(string_ex):
+    rs = []
+    collect_str = ''
+    for item in string_ex:
+        if item in oper:
+            rs.append(collect_str)
+            rs.append(item)
+            collect_str = ''
+        else:
+            collect_str+= item
+    rs.append(collect_str)
+    return rs
+
+def calc_dat_shit(s1, c1, c2, item):
+    if item == '+':
+        s1.append(c1+c2)
+    if item == '*':
+        s1.append(c1*c2)
+    if item == '-':
+        s1.append(c1-c2)
+    if item == '/':
+        s1.append(c2/c1)
+    return s1
+
 def c(input):
     s1 = []
     s2 = []
-    oper = ['+', '*']
 
     for item in input:
-
+        print(s1)
         if item in oper:
             if stack_is_empty(s2):
                 s2.append(item)
+
             else:
                 if priority[s2[-1]] < priority[item]:
                     c1 = int(s1.pop())
                     c2 = int(s1.pop())
 
-                    if item == '+':
-                        s1.append(c1+c2)
-                    if item == '*':
-                        s1.append(c1*c2)
+                    s1 = calc_dat_shit(s1, c1, c2, item)
+
+                elif priority[s2[-1]] == priority[item]:
+                    c1 = int(s1.pop())
+                    c2 = int(s1.pop())
+
+                    s1 = calc_dat_shit(s1, c1, c2, item)
+                    pass
                 else:
                     s2.append(item)
                     pass
@@ -46,12 +77,15 @@ def c(input):
             c1 = int(s1.pop())
             c2 = int(s1.pop())
             item = s2.pop()
-            if item == '+':
-                s1.append(c1+c2)
-            if item == '*':
-                s1.append(c1*c2)
 
-    print(s1, s2)
+            s1 = calc_dat_shit(s1, c1, c2, item)
+
+
+    return s1
     pass
 
-c(B)
+while 1:
+    s = input('enter calcer: ')
+    b = get_stack(s)
+    a = c(b)
+    print(b, s, "=",a)
