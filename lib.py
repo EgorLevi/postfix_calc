5ff83ddc5fd67d4f396cadf7524202e6b361af44
asd <asd@ubu.com>
2022-03-21 22:44:39 +0200

    created log database manager funcs

--- lib.py
+++ lib.py
@@ -1,1 +1,13 @@
-import random
+import sqlite3
+
+connection = sqlite3.connect()
+cursor = connection.cursor()
+def create_table():
+    cursor.execute("""CREATE TABLE IF NOT EXISTS calcss(input TEXT, result TEXT)""")
+    connection.commit()
+    return
+
+def insert_calc(expersion, result):
+    cursor.execute(f"""INSERT INTO calcss VALUES({expresion},{result})""")
+    connection.commit()
+    return
